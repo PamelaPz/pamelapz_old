7c81605897bbe1dba242c323a6e1dfc5b13e599f
PamelaPz <jpamelapz02@gmail.com>
2021-03-17 21:22:30 -0600

    maquetación de seccion name y welcome

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import './App.scss';
+import { Container, Row, Col } from 'react-bootstrap';
+import Menu from './Menu';
+
+function App() {
+  return (
+      <Container fluid>
+        <Row>
+          <Col lg="12">
+            <Menu/>
+          </Col>
+          <section className="section container content" id="name">
+            <Row className="name">
+              <Col lg="6">
+                <div className="wrapper_name">
+                  <div>
+                    <p className="super_title">Pamela <br /> Pérez</p>
+                  </div>
+                  <div>
+                    <span> <hr width="50%" /> <hr width="80%"/></span>
+                  </div>
+                  <div>
+                    <p className="super_subtitle">Maquetador / <br/>Desarrollador Frontend</p>
+                  </div>
+                </div>
+              </Col>
+              <Col lg="6">
+                <div className="wrapper_portfolio">
+                  <h1 className="title_section">Portafolio</h1>
+                </div>
+              </Col>
+            </Row>
+          </section>
+          <section className="section content container" id="welcome">
+            <h1 className="title">Bienvenido(a)</h1>
+            <div className="wrapper_text">
+            <p className="text">Geek de nacimiento y corazón, amante de los animales, la tecnología y la música. </p>
+            <p className="text">Mi principal objetivo se concentra en la creatividad, innovación y programación de ideas que posteriormente se puedan y deban desarrollar para solucionar necesidades del mundo en general.</p>
+            <p className="text">Procuro ponerme retos sin importar cuánto me tome lograrlo, porque intentar es lo que cuenta.</p>
+            <p className="text">Escribir más :v</p>
+            </div>
+          </section>
+          <section className="section content " id="skills">
+            Skills y Experiencia
+          </section>
+          <section className="section content" id="porfolio">
+            Portafolio / Enlaces
+          </section>
+          <footer> Footer </footer>
+        </Row>
+      </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,26 @@
+import './App.scss';
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
+
+function Menu() {
+  return (
+      <Container>
+        <Row>
+          <Col lg="12">
+            <Navbar bg="white" expand="lg" fixed="top">
+              <Navbar.Brand href="#home">Pamela.pz</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                  <Nav.Link href="#home">Inicio</Nav.Link>
+                  <Nav.Link href="#link">Experiencia</Nav.Link>
+                  <Nav.Link href="#link">Portafolio</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+          </Col>
+        </Row>
+      </Container>
+  );
+}
+
+export default Menu;
