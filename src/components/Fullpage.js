f5e505cb5c8fec3bd55365d7218184839f1ad36d
PamelaPz <jpamelapz02@gmail.com>
2021-03-24 18:51:50 -0600

    secciones terminadas

--- src/components/Fullpage.js
+++ src/components/Fullpage.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Menu from './Menu';
+import linkedin from '../images/linkedin.png';
+import instagram from '../images/instagram.png';
+import whatsapp from '../images/whatsapp.png';
 import terminal from '../images/terminal_pink.png';
 import ReactFullpage from '@fullpage/react-fullpage';
 
@@ -32,36 +35,63 @@ const Fullpage = () => (
             </section>
 
             <section className="section" id="quien">
-                <div className="wrapper column-12 colum-sm-12 row_reverse" >
-                  <div className="wrapper_name">
-                    <div>
-                      <p className="super_title">¿Quién Soy?</p>
-                    </div>
-                    <div className="wrapper_divider">
-                      <img src={terminal} alt=""/>
-                    </div>
-                    <div className="wrapper_subtitle p_wrap">
-                      <p className="super_subtitle">una chica que ama la tecnología, programar y ayudar a los demás.</p>
-                    </div>
-                    <div className="wrapper_btn">
-                      <a href="#i"><p>Saber más</p></a>
-                    </div>
+              <div className="wrapper column-12 colum-sm-12 row_reverse" >
+                <div className="wrapper_name">
+                  <div>
+                    <p className="super_title">¿Quién Soy?</p>
+                  </div>
+                  <div className="wrapper_divider">
+                    <img src={terminal} alt=""/>
+                  </div>
+                  <div className="wrapper_subtitle p_wrap">
+                    <p className="super_subtitle">una chica que ama la tecnología, programar y ayudar a los demás</p>
+                  </div>
+                  <div className="wrapper_btn">
+                    <a href="#i"><p>Saber más</p></a>
                   </div>
                 </div>
+                </div>
             </section>
-            {/* <section className="section" id="skills">
-              <div className="container">
-                Skills y Experiencia
+
+            <section className="section" id="portfolio">
+              <div className="wrapper column-12 colum-sm-12 row_reverse" >
+                <div className="wrapper_name">
+                  <div>
+                    <p className="super_title">Portafolio</p>
+                  </div>
+                  <div className="wrapper_divider">
+                    <img src={terminal} alt=""/>
+                  </div>
+                  <div className="wrapper_subtitle p_wrap">
+                    <p className="super_subtitle">mira algunos de los sitios que he desarrollado y personalizado</p>
+                  </div>
+                  <div className="wrapper_btn">
+                    <a href="#i"><p>Ir a mirar</p></a>
+                  </div>
+                </div>
               </div>
-            </section> */}
-            {/* <section className="section" id="porfolio">
-              <div className="container">
-                Portafolio / Enlaces
-                <button onClick={() => fullpageApi.moveSectionDown()}>
-                  Click me to move down
-                </button>
+            </section>
+
+            <section className="section" id="contact">
+              <div className="wrapper column-12 colum-sm-12 row_reverse" >
+                <div className="wrapper_name">
+                  <div>
+                    <p className="super_title">contáctame</p>
+                  </div>
+                  <div className="wrapper_divider ">
+                    <img src={terminal} alt="terminal"/>
+                  </div>
+                  <div className="wrapper_subtitle p_wrap mt-2">
+                    <p className="super_subtitle">jpamelapz02@gmail.com</p>
+                  </div>
+                  <div className="wrapper_socials">
+                    <a href="#i" className="icon_social"> <img src={whatsapp} alt="whatsapp"/></a>
+                    <a href="#i" className="icon_social"> <img src={instagram} alt="instagram"/></a>
+                    <a href="#i" className="icon_social"> <img src={linkedin} alt="linkedin"/></a>
+                  </div>
+                </div>
               </div>
-            </section> */}
+            </section>
           </ReactFullpage.Wrapper>
         );
       }}
