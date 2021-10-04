2f77185fd35869275f84aaed2c2e8b98a8966da2
PamelaPz <jpamelapz02@gmail.com>
2021-10-04 10:43:00 -0500

    proyecto proximo

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -151,6 +151,14 @@ function Portfolio() {
                             </Col>
                             <Col xs="12" lg="6">
                                 <div className="wrapper_cards ml-lg-4">
+                                    <div className="wrapper_card mr-auto">
+                                        <a href="http://capitalamigo.com.mx/" target="_black">
+                                            <div className="capa">
+                                                <div className="wp_sitio-name"><p className="title-p text-white">Proximamente</p></div>
+                                            </div>
+                                            <img src={proxi} alt="" />
+                                        </a>
+                                    </div>
                                     <div className="wrapper_card mr-auto first-top">
                                         <a href="https://proyecto-enviar-email.vercel.app" target="_black">
                                             <div className="capa">
